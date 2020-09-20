38d090da302bfd7962ccca9f6c97db04a1d447b2
MohitPatni <mohitpatni293@gmail.com>
2020-09-20 23:46:43 +0530

    package json change'

--- src/app/server.js
+++ src/app/server.js
@@ -5,9 +5,11 @@ const app = express();
 app.use(express.static('./dist/yogesh-painter-car-cares'));
 
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/yogesh-painter-car-care/index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile('index.html', { root: 'dist/yogesh-painter-car-cares' }
+    );
   });
+
 app.listen(process.env.PORT || 8080);
 
 console.log(`Running on port ${process.env.PORT || 8080}`)
